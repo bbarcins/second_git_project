e2ec9f81e788da779762dc8e877f697f9c5a877a
Bartosz <b.barcinski@gmail.com>
2018-04-20 22:37:26 +0200

    Initial commit

---
+++ src/second_git_project.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : second_git_project.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	/*GitHub Windows client*/
+	return EXIT_SUCCESS;
+}
